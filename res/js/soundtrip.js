5cb63fdb541f10a5a56be7b531cf08a30eac29a2
Paul Swift <paul@swiftace.com>
2014-11-04 22:45:35 +0000

    Interim commit.

--- res/js/soundtrip.js
+++ res/js/soundtrip.js
@@ -14,122 +14,207 @@
  limitations under the License.
  */
 
-// This ensures that the global scope has only one SoundTrip defined, making it act as a singleton.
-var SoundTrip = SoundTrip || {};
-
 // Immediately Invoked Function Expression (IIFE) to define and configure the SoundTrip object.
-(function() {
+// Uses the Singleton pattern, see http://addyosmani.com/resources/essentialjsdesignpatterns/book/#singletonpatternjavascript
+var SoundTrip = (function () {
     // Strict mode has intentionally different semantics to non-strict mode, and isn't supported in older browsers.
     'use strict';
 
-    /**
-     * Configuration hash of key/value pairs.
-     * Includes default configuration options.
-     *
-     * @type object
-     */
-    SoundTrip.config = {
-        resourcePath: '/resource/sound/',
-        ambient: {
-            path: 'ambient/',
-            multiShot: false,
-            loop: true,
-            queue: false,
-            crossFade: true
-        },
-        music: {
-            path: 'music/',
-            multiShot: false,
-            loop: false,
-            queue: true,
-            crossFade: false
-        },
-        effect: {
-            path: 'effect/',
-            multiShot: true,
-            loop: false,
-            queue: true,
-            crossFade: false
+    // Instance stores a reference to the Singleton
+    var instance;
+ 
+    function init() {
+
+        // Singleton
+
+        // Private methods and variables
+
+        /**
+         * Sound queue protoype.
+         * Protoype pattern: http://addyosmani.com/resources/essentialjsdesignpatterns/book/#prototypepatternjavascript
+         */
+        var SoundQueuePrototype = {
+
+            // Properties
+            type: '',
+            spec: {
+                path: '/',
+                multiShot: false,
+                loop: false,
+                queue: false,
+                crossFade: false
+            },
+            queue: {},
+
+            // Methods
+            push: function (soundId) {},
+            pauseOn: function () {},
+            pauseOff: function () {},
+            volume: function () {},
+            clear: function () {}
+
+        };
+
+        // Holds structured list of sounds
+        var resources = {};
+
+        // Holds sound queues
+        var soundQueues = [];
+
+        return {
+
+            // Public methods and variables
+
+            /**
+             * Configuration hash of key/value pairs.
+             * Includes default configuration options.
+             *
+             * @type object
+             */
+            config: {
+                resourcePath: '/res/sound/',
+                soundTypes: {
+                    ambient: {
+                        path: 'ambient/',
+                        multiShot: false,
+                        loop: true,
+                        queue: false,
+                        crossFade: true
+                    },
+                    music: {
+                        path: 'music/',
+                        multiShot: false,
+                        loop: false,
+                        queue: true,
+                        crossFade: false
+                    },
+                    effect: {
+                        path: 'effect/',
+                        multiShot: true,
+                        loop: false,
+                        queue: true,
+                        crossFade: false
+                    }
+                }
+            },
+
+            /**
+             * Set sound resources.
+             */
+            setResources: function (resources) {
+                this.resources = resources;
+
+                return this;
+            },
+
+            /**
+             * Get sound resources.
+             */
+            getResources: function () {
+                return this.resources;
+            },
+
+            /**
+             * Destroy queues and build afresh.
+             */
+            initialiseSoundQueues: function () {
+                // todo: Stop any currently playing sounds first!
+                this.soundQueues = [];
+
+                // Loop though this.config.soundTypes and create a queue for each
+
+                var basePath = this.config.resourcePath;
+                var soundTypes = this.config.soundTypes;
+                for (var key in soundTypes) {
+                    var q = this.SoundQueuePrototype;
+
+                    q.type = key;
+                    q.spec = soundTypes[key];
+                    q.spec.path = basePath + q.spec.path;
+
+                    this.soundQueues.push(q)
+                }
+            },
+
+            /**
+             * Initialise sound queues and return rendered sound board HTML based on resources and config.
+             */
+            createBoard: function () {
+                this.initialiseSoundQueues();
+
+                return '<p>Hello!</p>';
+            },
+
+            /**
+             * Play a sound (add to appropriate queue if a queue-able item).
+             */
+            soundPlay: function (soundId) {
+            },
+
+            /**
+             * Pause a sound.
+             */
+            soundPauseOn: function (soundId) {
+            },
+
+            /**
+             * Un-pause a paused sound.
+             */
+            soundPauseOff: function (soundId) {
+            },
+
+            /**
+             * Stop a sound (remove from queue if a queue-able item).
+             */
+            soundStop: function (soundId) {
+            },
+
+            /**
+             * Set a sound's volume as % of global volume.
+             */
+            soundVolume: function (soundId, volume) {
+            },
+
+            /**
+             * Pause all sounds.
+             */
+            allPauseOn: function () {
+            },
+
+            /**
+             * Un-pause all paused sounds.
+             */
+            allPauseOff: function () {
+            },
+
+            /**
+             * Stop all sounds (empty queues of queue-able items).
+             */
+            allStop: function (soundId) {
+            },
+
+            /**
+             * Set global volume as %.
+             */
+            allVolume: function (soundId, volume) {
+            },
+
+        };
+
+    }
+
+    return {
+ 
+        // Get the Singleton instance if one exists or create one if it doesn't
+        getInstance: function () {
+ 
+            if ( ! instance) {
+                instance = init();
+            }
+ 
+            return instance;
         }
+ 
     };
 
-    /**
-     * Sound queue protoype.
-     */
-    var SoundQueue = {};
-    SoundQueue.prototype.push = function(soundId) {
-    };
-
-    SoundQueue.prototype.pause = function(soundId) {
-    };
-
-    SoundQueue.prototype.clear = function(soundId) {
-    };
-
-    /**
-     * Add sound resource data.
-     */
-    SoundTrip.addResources = function() {
-    };
-
-    /**
-     * Render sound board HTML based on resources.
-     */
-    SoundTrip.renderBoard = function() {
-    };
-
-    /**
-     * Play a sound (add to appropriate queue if a queue-able item).
-     */
-    SoundTrip.soundPlay = function(soundId) {
-    };
-
-    /**
-     * Pause a sound.
-     */
-    SoundTrip.soundPauseOn = function(soundId) {
-    };
-
-    /**
-     * Un-pause a paused sound.
-     */
-    SoundTrip.soundPauseOff = function(soundId) {
-    };
-
-    /**
-     * Stop a sound (remove from queue if a queue-able item).
-     */
-    SoundTrip.soundStop = function(soundId) {
-    };
-
-    /**
-     * Set a sound's volume as % of global volume.
-     */
-    SoundTrip.soundVolume = function(soundId, volume) {
-    };
-
-    /**
-     * Pause all sounds.
-     */
-    SoundTrip.allPauseOn = function() {
-    };
-
-    /**
-     * Un-pause all paused sounds.
-     */
-    SoundTrip.allPauseOff = function() {
-    };
-
-    /**
-     * Stop all sounds (empty queues of queue-able items).
-     */
-    SoundTrip.allStop = function(soundId) {
-    };
-
-    /**
-     * Set global volume as %.
-     */
-    SoundTrip.allVolume = function(soundId, volume) {
-    };
-
-}());
+})();
